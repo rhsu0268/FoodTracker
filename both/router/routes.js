93ea439b37822bc0a7fc6af389d80b2e157a4cfb
Richard Hsu <rhsu0268@gmail.com>
2016-08-06 17:48:44 -0400

    Adding back files

---
+++ both/router/routes.js
@@ -0,0 +1,14 @@
+Router.route('/', {
+  name: 'home'
+});
+
+Router.route('/dashboard', {
+  name: 'dashboard',
+  controller: 'DashboardController'
+});
+
+Router.plugin('ensureSignedIn', {
+  only: ['dashboard']
+});
+
+Router.route('/profile', { name: 'Profile' });
