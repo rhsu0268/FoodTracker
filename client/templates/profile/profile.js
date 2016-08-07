4a3101fcbae5c9f2e80ea06dcbb44e301e388776
Richard Hsu <rhsu0268@gmail.com>
2016-08-07 15:57:39 -0400

    Added client side code to add user

--- client/templates/profile/profile.js
+++ client/templates/profile/profile.js
@@ -6,35 +6,32 @@ Template.Profile.events({
         console.log("Updating profile!");
     }
 
-    /*
+
     // get each of the fields
-       var firstName = $('#firstName').val();
-       var lastName = $('#lastName').val();
-       var email = $('#email').val();
-       var password = $('#password').val();
-       var roles = $('#roles').val();
-       var territories = $('#territories').val();
-       //console.log(firstName);
+    var name = $('#name').val();
+    var age = $('#age').val();
+    var height = $('#height').val();
+    var weight = $('#weight').val();
+    //console.log(firstName);
 
-       var newUser = {
-           userFirstName: firstName,
-           userLastName: lastName,
-           userEmail: email,
-           userPassword: password,
-           userRoles: roles,
-           userTerritories: territories
-       };
+    var newProfile = {
+        userName: name,
+        userAge: age,
+        userHeight: height,
+        userWeight weight,
 
-       // call the create user
-       Meteor.call('addUser', newUser, function (error, result) {
-           console.log("Calling addUser");
-           if (error)
-           {
-               console.log(error.reason);
-           }
-           console.log("User created!");
+    };
 
-       });
-       */
+    // call the create user
+    Meteor.call('updateProfile', newProfile, function (error, result) {
+        console.log("Calling updateProfile");
+        if (error)
+        {
+            console.log(error.reason);
+        }
+        console.log("Profile updated!");
+
+    });
+
 
 });
